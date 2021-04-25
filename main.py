b02ac10e850867ad41ee50af6fab2eb8ffcbff38
Dmitry <dimk00z@gmail.com>
2021-04-25 16:36:29 +0300

    add a plaint text with sanitazer

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
 import aiohttp
 import asyncio
 
+from urllib.parse import urlparse
 
-async def fetch(session, url):
+from adapters import SANITIZERS
+
+
+def extract_sanitazer_name(url: str) -> str:
+    domain: str = urlparse(url).netloc
+    return '_'.join(domain.split('.'))
+
+
+async def fetch(session: aiohttp.client.ClientSession,
+                url: str) -> str:
     async with session.get(url) as response:
         response.raise_for_status()
         return await response.text()
 
 
 async def main():
+    url: str = 'https://inosmi.ru/social/20210424/249625353.html'
+    sanitazer_name: str = extract_sanitazer_name(url=url)
+    sanitizer: function = SANITIZERS.get(sanitazer_name)
+
     async with aiohttp.ClientSession() as session:
-        html = await fetch(session, 'http://example.com')
-        print(html)
+        html: str = await fetch(session, url)
+        print(sanitizer(html, plaintext=True))
 
-
-asyncio.run(main())
+if __name__ == '__main__':
+    asyncio.run(main())
