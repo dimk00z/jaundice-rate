c81d31691ae04ecc327ca99a79b550e0a4cb8352
Dmitry <dimk00z@gmail.com>
2021-07-16 14:41:11 +0300

    remove trash code

--- server.py
+++ server.py
@@ -10,24 +10,6 @@ from utils.utils import load_dictionaries
 logger = logging.getLogger('server')
 
 
-async def handle(request):
-
-    if request.rel_url.query.get('urls') is None:
-        return web.json_response({
-            "error": "no one url requested"
-        }, status=400)
-
-    urls = request.rel_url.query['urls'].split(',')
-
-    if len(urls) > 10:
-        return web.json_response({
-            "error": "too many urls in request, should be 10 or less"
-        }, status=400)
-
-    return web.json_response({
-        'urls': urls})
-
-
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
     logger.setLevel(logging.DEBUG)
